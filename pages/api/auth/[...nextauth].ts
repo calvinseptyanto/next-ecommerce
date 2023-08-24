e24286e8a7ecb3d765b7cdbab57d57a8d65d4e68
calvinseptyanto <98633109+calvinseptyanto@users.noreply.github.com>
2023-08-25 00:24:14 +0800

    Added Navbar

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,7 +5,7 @@ import { PrismaClient } from "@prisma/client";
 import Stripe from "stripe";
 const prisma = new PrismaClient();
 
-export default NextAuth({
+export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -30,4 +30,6 @@ export default NextAuth({
       }
     },
   },
-});
+};
+
+export default NextAuth(authOptions);
