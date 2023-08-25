9023fda92e3ff294291f37753b062dd7b5ac54d5
calvinseptyanto <98633109+calvinseptyanto@users.noreply.github.com>
2023-08-25 23:39:55 +0800

    Styled the home page

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -3,10 +3,20 @@ import formatPrice from "@/util/PriceFormat";
 import { ProductType } from "@/types/ProductType";
 export default function Product({ name, image, price }: ProductType) {
   return (
-    <div>
-      <Image src={image} alt={name} width={400} height={400} />
-      <h1>{name}</h1>
-      {formatPrice(price as number)}
+    <div className="text-gray-700">
+      <Image
+        src={image}
+        alt={name}
+        width={800}
+        height={800}
+        className="w-full h-96 object-cover rounded-lg"
+      />
+      <div className="font-medium py-2">
+        <h1>{name}</h1>
+        <h2 className="text-sm text-teal-700">
+          {price && formatPrice(price as number)}
+        </h2>
+      </div>
     </div>
   );
 }
